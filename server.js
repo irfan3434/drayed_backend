be4a92f2efe8ba6f01c11d7cbb8599d8d7aacedf
Irfan Ishtiaq <irfan.ishtiaq@fcec.sa>
2025-01-02 11:10:05 +0300

    new changes in the mongo code

--- server.js
+++ server.js
@@ -365,7 +365,7 @@ app.post('/submit-application', uploadMiddleware, async (req, res) => {
 
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Database connected successfully!'))
   .catch(err => {
     console.error('Database connection error:', err);
